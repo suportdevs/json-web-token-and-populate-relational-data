66c7f247f88344d95fd40b92de692e90c72c6344
suportdevs <suportdevs@gmail.com>
2022-08-26 09:51:51 +0600

    successfully create user login and generate jwt token

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userSchema = require('../schemas/userSchema');
 const router = express.Router();
 
@@ -28,9 +29,16 @@ router.post('/login', async(req, res) => {
     try{
         const user = await User.find({username: req.body.username});
         if(user && user.length > 0){
-            const isValidPassword = bcrypt.compare(req.body.password, user[0].password);
+            const isValidPassword = await bcrypt.compare(req.body.password, user[0].password);
             if(isValidPassword){
-                
+                const token = await jwt.sign({
+                    username: user[0].username,
+                    userId: user[0]._id
+                }, process.env.JWT_SECRET, {
+                    expiresIn: '1h'
+                });
+
+                res.status(200).json({access_token: token, message: 'Authentication successfull.'})
             } else {
                 res.status(401).json({'error': 'Authentication failed!'});
             }
@@ -38,7 +46,7 @@ router.post('/login', async(req, res) => {
             res.status(401).json({'error': 'Authentication failed!'});
         }
     } catch(err) {
-
+        res.status(401).json({'error': 'Authentication failed!' + err});
     }
 })
 
